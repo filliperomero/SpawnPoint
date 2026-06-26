27327e020d324aed09aa0c928b4dbf08d783f53d
filliperomero <fillipe.romero@gmail.com>
2026-06-25 23:17:29 -0600

    Add Weapon class

---
+++ Source/SpawnPoint/Private/Weapons/SP_Weapon.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Weapons/SP_Weapon.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+ASP_Weapon::ASP_Weapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true;
+	
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh1P");
+	Mesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh1P->bReceivesDecals = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetHiddenInGame(true);
+	SetRootComponent(Mesh1P);
+	
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh3P");
+	Mesh3P->SetupAttachment(Mesh1P);
+	Mesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh3P->bReceivesDecals = false;
+	Mesh3P->CastShadow = true;
+	Mesh3P->SetHiddenInGame(true);
+}
+
+void ASP_Weapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/SpawnPoint/Public/Weapons/SP_Weapon.h
@@ -0,0 +1,32 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SP_Weapon.generated.h"
+
+UCLASS()
+class SPAWNPOINT_API ASP_Weapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASP_Weapon();
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	/** Weapon Mesh: 1st Person View */
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+	
+	/** Weapon Mesh: 3rd Person View */
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+	
+public:
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	USkeletalMeshComponent* GetMesh3P() const { return Mesh3P; }
+};
