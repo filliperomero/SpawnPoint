b185419a2973c291ecd792ff4c18f58a88487f79
filliperomero <fillipe.romero@gmail.com>
2026-08-04 23:28:42 -0600

    Add backbone of the player state

---
+++ Source/SpawnPoint/Private/Player/SP_PlayerState.cpp
@@ -0,0 +1,90 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Player/SP_PlayerState.h"
+
+ASP_PlayerState::ASP_PlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
+
+void ASP_PlayerState::AddScoredElim()
+{
+	++ScoredElims;
+}
+
+void ASP_PlayerState::AddDefeat()
+{
+	++Defeats;
+}
+
+void ASP_PlayerState::AddHit()
+{
+	++Hits;
+}
+
+void ASP_PlayerState::AddMiss()
+{
+	++Misses;
+}
+
+void ASP_PlayerState::AddHeadShotElim()
+{
+	++HeadShotElims;
+}
+
+void ASP_PlayerState::AddSequentialElim(int32 SequenceCount)
+{
+	if (SequentialElims.Contains(SequenceCount))
+	{
+		SequentialElims[SequenceCount]++;
+	}
+	else
+	{
+		SequentialElims.Add(SequenceCount, 1);
+	}
+	
+	/**
+	 * Reduce the count for all lower sequence counts. This is because a triple elim means a double was scored first.
+	 * But we want to count this as just a triple, i.e.: elim 1, elim 2, elim 3 = just a triple, not a double and a triple
+	 */
+	for (auto& Element : SequentialElims)
+	{
+		if (Element.Key < SequenceCount && Element.Value > 0)
+		{
+			Element.Value--;
+		}
+	}
+}
+
+void ASP_PlayerState::UpdateHighestStreak(int32 StreakCount)
+{
+	if (StreakCount > HighestStreak)
+	{
+		HighestStreak = StreakCount;
+	}
+}
+
+void ASP_PlayerState::AddRevengeElim()
+{
+	++RevengeElims;
+}
+
+void ASP_PlayerState::AddDethroneElim()
+{
+	++DethroneElims;
+}
+
+void ASP_PlayerState::AddShowStopperElim()
+{
+	++ShowStopperElims;
+}
+
+void ASP_PlayerState::GotFirstBlood()
+{
+	bFirstBlood = true;
+}
+
+void ASP_PlayerState::IsNowWinner()
+{
+	bWinner = true;
+}
---
+++ Source/SpawnPoint/Public/Player/SP_PlayerState.h
@@ -0,0 +1,44 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "SP_PlayerState.generated.h"
+
+UCLASS()
+class SPAWNPOINT_API ASP_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	ASP_PlayerState();
+
+	void AddScoredElim();
+	void AddDefeat();
+	void AddHit();
+	void AddMiss();
+	void AddHeadShotElim();
+	void AddSequentialElim(int32 SequenceCount);
+	void UpdateHighestStreak(int32 StreakCount);
+	void AddRevengeElim();
+	void AddDethroneElim();
+	void AddShowStopperElim();
+	void GotFirstBlood();
+	void IsNowWinner();
+	
+private:
+	int32 ScoredElims { 0 };
+	int32 Defeats { 0 };
+	int32 Hits { 0 };
+	int32 Misses { 0 };
+	bool bOnStreak { false }; /* How many elims do we have since we've spawned? */
+	int32 HeadShotElims { 0 };
+	TMap<int32, int32> SequentialElims; /* Sequential elims - multiple elims within a short period of time */
+	int32 HighestStreak { 0 };
+	int32 RevengeElims { 0 };
+	int32 DethroneElims { 0 };
+	int32 ShowStopperElims { 0 };
+	bool bFirstBlood { false };
+	bool bWinner { false };
+};
