671780a24bd2b84fc2f4fbcb0da723f01ee14fab
filliperomero <fillipe.romero@gmail.com>
2026-08-09 00:54:06 -0600

    Add Special Elimination Widget

---
+++ Source/SpawnPoint/Private/UI/Eliminations/SP_SpecialEliminationWidget.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+
+#include "UI/Eliminations/SP_SpecialEliminationWidget.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void USP_SpecialEliminationWidget::InitializeWidget(const FString& ElimMessage, UTexture2D* ElimTexture)
+{
+	if (IsValid(Text_ElimMessage))
+	{
+		Text_ElimMessage->SetText(FText::FromString(ElimMessage));
+	}
+	
+	if (IsValid(Image_ElimType))
+	{
+		Image_ElimType->SetBrushFromTexture(ElimTexture);
+	}
+}
+
+void USP_SpecialEliminationWidget::CenterWidget(UUserWidget* Widget, float VerticalRatio)
+{
+	if (!IsValid(Widget)) return;
+
+	FVector2D ViewportSize = UWidgetLayoutLibrary::GetViewportSize(Widget);
+	const float VerticalFraction = VerticalRatio == 0.f ? 1.f : VerticalRatio * 2.f;
+	FVector2D CenterPosition(ViewportSize.X / 2.f, VerticalFraction * ViewportSize.Y / 2.f);
+	
+	Widget->SetAlignmentInViewport(FVector2D(0.5f, 0.5f)); // Align widget center to the center of the viewport
+	Widget->SetPositionInViewport(CenterPosition, true);
+}
---
+++ Source/SpawnPoint/Public/UI/Eliminations/SP_SpecialEliminationWidget.h
@@ -0,0 +1,28 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SP_SpecialEliminationWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class SPAWNPOINT_API USP_SpecialEliminationWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> Text_ElimMessage;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Image_ElimType;
+	
+	void InitializeWidget(const FString& ElimMessage, UTexture2D* ElimTexture);
+	
+	UFUNCTION(BlueprintCallable)
+	static void CenterWidget(UUserWidget* Widget, float VerticalRatio = 0.f);
+};
